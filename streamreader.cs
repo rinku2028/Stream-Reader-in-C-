d2207b5e3a3f2bc2ee96d1d76cf50b0642dfc3db
rinku2028 <40994882+rinku2028@users.noreply.github.com>
2020-01-30 00:22:12 +0545

    Add files via upload

---
+++ streamreader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace streamreader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = @"E:\testfile.txt"; //location of the file to read
+
+            using (StreamReader reader = File.OpenText(path))
+            {
+                String str = "";
+
+                while ((str = reader.ReadLine()) != null)
+                {
+                    Console.WriteLine(str);//Read the contents of the file and prints in console
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+    }
